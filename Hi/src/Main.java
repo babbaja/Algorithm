462d29442d2c02c1d0bb3d192337e3314362c824
babbaja <cho00070265@gmail.com>
2024-01-09 15:33:15 +0900

    first commit

--- Hi/src/Main.java
+++ Hi/src/Main.java
@@ -1,23 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[M][2];
-
-        for (int i = 0; i < M; ++i) {
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-        }
-
-
-
-
+    public static void main(String[] args) {
+        System.out.println("Hi");
     }
 }
