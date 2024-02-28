51969e0252d62724f1cd43817a15669c96a6f740
babbaja <cho00070265@gmail.com>
2024-02-28 17:09:39 +0900

    1987

--- alphabet_1987/src/Main.java
+++ alphabet_1987/src/Main.java
@@ -4,41 +4,42 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int R, C;
-    static int[][] list;
-    static boolean[] alpa = new boolean[26];
-    static int ans;
-
+    public static int[][] arr;
+    public static int[] dx = new int[] {-1 ,0, 1, 0};
+    public static int[] dy = new int[] {0, -1, 0, 1};
+    public static boolean[] alpha = new boolean[26];
+    public static int ans = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        list = new int[R][C];
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        arr = new int[R][C];
 
         for (int i = 0; i < R; ++i) {
-            String str = br.readLine();
+            char[] list = br.readLine().toCharArray();
             for (int j = 0; j < C; ++j) {
-                list[i][j] = str.charAt(j) - 'A';
+                arr[i][j] = list[j] - 'A';
             }
         }
-        alphabet(0, 0, 0);
+        alpha[arr[0][0]] = true;
+        alphabet(0, 0, 1);
         System.out.println(ans);
     }
 
-    static void alphabet(int cnt, int row, int cols) {
-        if (alpa[list[row][cols]]) {
-            ans = Math.max(ans, cnt);
-            return;
-        }
-        else {
-            alpa[list[row][cols]] = true;
-            if (row > 0) alphabet(cnt + 1, row - 1, cols);
-            if (cols > 0) alphabet(cnt + 1, row, cols - 1);
-            if (row < R - 1) alphabet(cnt + 1, row + 1, cols);
-            if (cols < C - 1) alphabet(cnt + 1, row, cols + 1);
-            alpa[list[row][cols]] = false;
+    public static void alphabet(int x, int y, int cnt) {
+        ans = Math.max(ans, cnt);
+
+        for (int i = 0; i < 4; ++i) {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+
+            if (nextX >= 0 && nextX < arr.length && nextY >= 0 && nextY < arr[0].length && !alpha[arr[nextX][nextY]]) {
+                alpha[arr[nextX][nextY]] = true;
+                alphabet(nextX, nextY, cnt + 1);
+                alpha[arr[nextX][nextY]] = false;
+            }
         }
     }
 }
